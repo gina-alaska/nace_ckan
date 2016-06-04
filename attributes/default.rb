e40b74efbe642441b8cfdc2c2cee7514952a9a3a
Bob Torgerson <rltorgerson@alaska.edu>
2016-06-03 16:48:58 -0800

    Attributes to define the production.ini template. Defaults still match original design, so should not affect our development Vagrant installs.

---
+++ attributes/default.rb
@@ -0,0 +1,6 @@
+default['ckan']['db_name'] = "ckan"
+default['ckan']['db_datastore_name'] = "datastore_default"
+default['ckan']['db_username'] = "ckan_default"
+default['ckan']['db_password'] = "pass"
+default['ckan']['db_address'] = "localhost"
+default['ckan']['site_url'] = "localhost"
