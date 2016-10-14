1f56fb53ca2244c10ae4091a887e12ec993f4bfb
Bob Torgerson <rltorgerson@alaska.edu>
2016-10-14 14:54:17 -0800

    Add new CKAN extension for allowing groups to view private datasets.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'support+chef@gina.alaska.edu'
 license 'mit'
 description 'Installs/Configures nace-ckan'
 long_description 'Installs/Configures nace-ckan'
-version '1.0.1'
+version '1.0.2'
 
 depends 'httpd', '= 0.3.5'
 depends 'solr', '~> 0.5.0'
