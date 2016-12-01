173866d413a13c83b514b26c806e78f56bcd975f
Will Fisher <will@gina.alaska.edu>
2016-12-01 11:45:59 -0900

    bump version to 1.2.11

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'support+chef@gina.alaska.edu'
 license 'mit'
 description 'Installs/Configures nace-ckan'
 long_description 'Installs/Configures nace-ckan'
-version '1.2.10'
+version '1.2.11'
 
 depends 'httpd'
 depends 'solr'
